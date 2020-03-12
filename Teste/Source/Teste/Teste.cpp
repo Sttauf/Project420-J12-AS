51dc6ce655c9c784cdebff1e31f4c5598d236bc4
Sarah <sttauf@yahoo.com.br>
2020-03-11 21:57:36 -0400

    +NewProject

---
+++ Teste/Source/Teste/Teste.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "Teste.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Teste, "Teste" );
+
+DEFINE_LOG_CATEGORY(LogTeste)
+ 
---
+++ Teste/Source/Teste/TesteGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TesteGameMode.generated.h"
+
+UCLASS(MinimalAPI)
+class ATesteGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATesteGameMode();
+};
+
+
+
